8a3222f7ddba17cb8f0d4f1e5aca2bceddd63c25
Виктория Разнодумова <raznodumova@gmail.com>
2024-11-30 20:03:32 +0300

    token eventually fixed

--- src/main/java/com/javarush/telegram/TinderBoltApp.java
+++ src/main/java/com/javarush/telegram/TinderBoltApp.java
@@ -9,13 +9,31 @@ import org.telegram.telegrambots.meta.api.objects.*;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class TinderBoltApp extends MultiSessionTelegramBot {
     public static final String TELEGRAM_BOT_NAME = "javarush_dating_ai_bot"; //TODO: добавь имя бота в кавычках
-    public static final String TELEGRAM_BOT_TOKEN = ""; //TODO: добавь токен бота в кавычках
-    //в resources создать файл config.properties
+    public static final String TELEGRAM_BOT_TOKEN; //TODO: добавь токен бота в кавычках
+    static{
+        TELEGRAM_BOT_TOKEN = getToken();
+    }
     public static final String OPEN_AI_TOKEN = "chat-gpt-token"; //TODO: добавь токен ChatGPT в кавычках
+
+    public static String getToken(){
+        Properties prop = new Properties();
+        try {
+            //load a properties file from class path, inside static method
+            prop.load(TinderBoltApp.class.getClassLoader().getResourceAsStream("config.properties"));
+            //get the property value and print it out
+            return prop.getProperty("token");
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException("Не удалось загрузить token из config.properties", ex);
+        }
+    }
 
     public TinderBoltApp() {
         super(TELEGRAM_BOT_NAME, TELEGRAM_BOT_TOKEN);
